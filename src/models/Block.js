d47b74c8a69667653dfa47a304b8be09829718f4
Caos <caosbad@gmail.com>
2023-03-05 16:56:29 +0800

    update file

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,5 +1,7 @@
 import sha256 from 'crypto-js/sha256'
 
+const DIFFICULTY = 2;
+
 // 区块
 class Block {
   constructor(
@@ -10,6 +12,26 @@ class Block {
     this.blockchain = blockchain
     this.nonce = nonce
     this.parentHash = parentHash
+    this.hash = sha256(this.nonce + this.parentHash).toString()
+  }
+
+  // 验证区块 hash
+  isValid() {
+    return (
+      this.parentHash === 'root' ||
+      (this.hash.substr(-DIFFICULTY) === '0'.repeat(DIFFICULTY) &&
+        this.hash === sha256(this.nonce + this.parentHash).toString())
+    )
+  }
+
+  // 设置 nonce 随机数
+  setNonce(nonce) {
+    this.nonce = nonce
+    this._setHash()
+  }
+
+  // 设置 Hash
+  _setHash() {
     this.hash = sha256(this.nonce + this.parentHash).toString()
   }
 }
